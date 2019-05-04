741a6b640d97f5e60a7abbb571e8ce1d23669c00
Italo Fasanelli <fasanelli.italo@gmail.com>
2019-05-04 14:48:06 -0300

    Criação do .js

---
+++ app/assets/javascripts/battleship.js
@@ -0,0 +1,64 @@
+const battleship = {
+
+    // ATRIBUTOS
+    board: ['','','','','','','','','','',
+            '','','','','','','','','','',
+            '','','','','','','','','','',
+            '','','','','','','','','','',
+            '','','','','','','','','','',
+            '','','','','','','','','','',
+            '','','','','','','','','','',
+            '','','','','','','','','','',
+            '','','','','','','','','','',
+            '','','','','','','','','',''],
+
+    board2: ['','','','','','','','','','',
+            '','','','','','','','','','',
+            '','','','','','','','','','',
+            '','','','','','','','','','',
+            '','','','','','','','','','',
+            '','','','','','','','','','',
+            '','','','','','','','','','',
+            '','','','','','','','','','',
+            '','','','','','','','','','',
+            '','','','','','','','','',''],
+
+    container_element: null,
+    gameover: false,
+      
+    // FUNÇÕES
+    init: function(container) {
+        this.container_element = container;
+    },
+
+    init1: function(container) {
+        this.container_element1 = container;
+    },
+
+    start: function() {
+        //this.board.fill('');
+        this.draw();
+        this.gameover = false;
+        alert("Clique no segundo tabuleiro para posicionar seus navios");     
+    },
+
+    game_is_over: function() {
+        this.gameover = true;
+        console.log('GAME OVER');
+        alert('GAME OVER')
+    },    
+
+    draw: function() {
+        let content = '';
+        let content1 = '';
+        for ( i in this.board ) {
+            content += '<div class="divs" onclick="battleship.jogadas(' + i + ')">' + this.board[i] + '</div>';
+        };
+        this.container_element.innerHTML = content;
+        for ( i in this.board2 ) {
+            content1 += '<div class="divs" onclick="battleship.jogadas(' + i + ')">' + this.board2[i] + '</div>';
+        };
+        this.container_element1.innerHTML = content1;
+        //this.container_element.style.visibility = hidden;
+    },
+};
