d64bef4829ec8c9f6f7edbb48ffa8f5344edc1f5
Ariel Neumeyer <arielneu@outlook.com>
2019-05-14 18:59:32 +0000

    paginas placa e index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/home" => "main#home"
-   get "/cadastro" => "main#cadastro"
+  get "/index" => "main#index"
+  get "/cadastro" => "main#cadastro"
+  get "/placar" => "main#placar"
 end
 
 #teste
