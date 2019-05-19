5272416e5872c3d4a899907907a81a56b503bba8
Eichler <felipe_eichler@hotmail.com>
2019-05-19 17:30:22 +0000

    Atualização no Banco de Dados

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
-  has_secure_password
+    VALID_EMAIL = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+    validates :email, presence: true, length: { minimum: 5 },
+    format: { with: VALID_EMAIL }, uniqueness: true
+    has_secure_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   #get 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/home" => "main#home"
- # get "/index" => "main#index"
+  get "/index" => "main#index"
   #get "/cadastro" => "main#cadastro"
   get "/placar" => "main#placar"
   get "/jogo" => "main#jogo"
