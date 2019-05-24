d43ac77065a5043fb84cb567cef76a87a05c43d6
Eichler <felipe_eichler@hotmail.com>
2019-05-24 20:26:19 +0000

    Nova mensagem de validação formulário cadastro

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
     VALID_EMAIL = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-    validates :email, presence: true, length: { minimum: 5 },
-    format: { with: VALID_EMAIL }, uniqueness: true
-    validates :password, presence: true, length: { minimum: 5},
-    uniqueness: false
-    validates :nome, presence: true, length: { minimum: 0 },
-    uniqueness: false
+    validates :email, presence: true, length: { minimum: 5 },   #Validação e-mail
+    format: { with: VALID_EMAIL }, uniqueness: true 
+    
+    validates :password, presence: true, length: { minimum: 5}, uniqueness: false  #validação senha
     has_secure_password
+    
+    validates :nome, presence: true, length: { minimum: 0 }, uniqueness: false    #validação nome
 end
