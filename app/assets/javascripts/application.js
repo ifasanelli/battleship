312389816a87ec14040a0e35b52f5a3ca8279c9f
Ariel Neumeyer <arielneu@outlook.com>
2019-05-24 13:49:41 +0000

    inserção img-levels

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,9 +9,16 @@ $(main);
 
 function main(){
     $('main').delay(200).slideDown(400);
+    
+    $('.dif').hover(function(){
+       var id = $(this).attr('id');
+       $('.img-level').attr('src','assets/bomb-'+id+'.png');
+    })
+    //-----------------------------------------
     $('.dif').click(function(){
         $('.modals, h2').fadeOut(400,function(){
             $('.game').css('display','grid');
         });
     })
+    //------------------------------------------
 }
