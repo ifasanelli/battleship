3c2b8cbf9221c63927f6c7ba36bc143d2540816c
Ariel Neumeyer <arielneu@outlook.com>
2019-06-02 12:31:51 +0000

    mais ajustes css

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,8 +8,10 @@
 $(main);
 
 function main(){
-    $('main, nav').delay(200).slideDown(400, function(){
+    $('.main-containers').delay(200).slideDown(400, function(){
         $(this).css('display','flex')
+        var mainHeight = $('header').height()
+        $(this).css('margin-top',mainHeight)
     });
     
     $('li').hover(function(){
@@ -28,19 +30,3 @@ function main(){
     
     
 }
-
-//  function alert_set_ship(alerta){
-//     $('.hint').html(alerta)
-//     $('.modals-set-ship').slideDown(400)
-//      $('.orientacao').click(function(){
-//          $('.modals-set-ship').slideUp(400)
-        
-//      })
-//  }
- 
-//  function set_ship(){
-//      var or = '';
-//       $('.orientacao').click(function(){
-//       or = $(this).attr('id') 
-//       })
-//  }
